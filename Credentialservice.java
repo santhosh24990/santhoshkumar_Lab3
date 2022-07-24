1c8009f038355665e73251e81e3a52ce0940ecd3
santhosh24990 <88759789+santhosh24990@users.noreply.github.com>
2022-07-24 14:36:40 +0530

    Delete Credentialservice.java

--- Credentialservice.java
+++
@@ -1,7 +0,0 @@
-package com.abc.email.model.service;
-import com.abc.email.model.Employee;
-public interface Credentialservice {
-	public String generateEmailAddress(Employee emp,String department);
-	public char[] generatePassword();
-	public void showCredential(Employee emp, String email, char[] generatePassword);
-}
